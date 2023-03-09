b6ed6ae8879d732be48139aab3087d024c393c49
wlad184 <evel184@yandex.ru>
2023-03-09 17:17:43 +0700

    OOП Интерфейсы и полиморфизм

--- src/ServiceStation.java
+++ src/ServiceStation.java
@@ -1,5 +1,6 @@
 public class ServiceStation {
     public void check(Car car, Bicycle bicycle, Truck truck) {
+
         if (car != null) {
             System.out.println("Обслуживаем " + car.getModelName());
             for (int i = 0; i < car.getWheelsCount(); i++) {
